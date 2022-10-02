fcc13ccb18c834bbe4c112d10e9551bf1de58643
JustSimon01 <simon.ch@yandex.ru>
2022-10-02 19:23:00 +0300

    index остались section и "можно лучше"

--- src/components/index.js
+++ src/components/index.js
@@ -40,36 +40,42 @@ let userId = "";
 const setUser = new UserInfo(userInfoSelectors);
 //открытие попапа редактирования профиля
 popupProfileEditButton.addEventListener("click", () => {
-  submitUser.open();
+  profilePopup.open();
   const { name, about } = setUser.getUserInfo();
   nameInput.value = name;
   jobInput.value = about;
-  userInfo.resetValidation();
+  profileFormValidator.resetValidation();
 });
 //открытие попапа добавления карточек
 popupProfileAddButton.addEventListener("click", () => {
-  submitButton.open();
-  addCard.resetValidation();
+  cardPopup.open();
+  addCardFormValidator.resetValidation();
 });
 
 //открытие попапа смены аватара
 popupProfileImageEditButton.addEventListener("click", () => {
   submitAvatar.open();
-  avatarChange.resetValidation();
+  avatarFromValidator.resetValidation();
 });
 
 //новая валидация полей
-const userInfo = new FormValidator(popupSelectorClass, formElementEditProfile);
-userInfo.enableValidation();
+const profileFormValidator = new FormValidator(
+  popupSelectorClass,
+  formElementEditProfile
+);
+profileFormValidator.enableValidation();
 
-const addCard = new FormValidator(popupSelectorClass, formElementAddCards);
-addCard.enableValidation();
+const addCardFormValidator = new FormValidator(
+  popupSelectorClass,
+  formElementAddCards
+);
+addCardFormValidator.enableValidation();
 
-const avatarChange = new FormValidator(
+const avatarFromValidator = new FormValidator(
   popupSelectorClass,
   formElementEditImageProfile
 );
-avatarChange.enableValidation();
+avatarFromValidator.enableValidation();
 
 //добавление карточки
 const openImage = new PopupWithImage(popupImage);
@@ -115,9 +121,9 @@ function createCard(item) {
   return cardElement;
 }
 
-const submitButton = new PopupWithForm(popupAddCards, {
+const cardPopup = new PopupWithForm(popupAddCards, {
   submitFormCallBack: (formData) => {
-    submitButton.renderLoading(true);
+    cardPopup.renderLoading(true);
     api
       .postAddCard(formData.place, formData.url)
       .then((cards) => {
@@ -132,30 +138,30 @@ const submitButton = new PopupWithForm(popupAddCards, {
           container
         );
         cardInitial.renderer();
-        submitButton.close();
+        cardPopup.close();
       })
       .catch((err) => console.log(err))
-      .finally(() => submitButton.renderLoading(false));
+      .finally(() => cardPopup.renderLoading(false));
   },
 });
-submitButton.setEventListeners();
+cardPopup.setEventListeners();
 
 //имя и работа в профиле
-const submitUser = new PopupWithForm(popupEditProfile, {
+const profilePopup = new PopupWithForm(popupEditProfile, {
   submitFormCallBack: (formData) => {
-    submitUser.renderLoading(true);
+    profilePopup.renderLoading(true);
     api
       .patchProfile(formData.name, formData.occupation)
       .then((res) => {
         setUser.setUserInfo(res);
-        submitUser.close();
+        profilePopup.close();
       })
       .catch((err) => console.log(err))
-      .finally(() => submitUser.renderLoading(false));
+      .finally(() => profilePopup.renderLoading(false));
   },
 });
 
-submitUser.setEventListeners();
+profilePopup.setEventListeners();
 
 //смена аватара
 const submitAvatar = new PopupWithForm(popupEditImageProfile, {
